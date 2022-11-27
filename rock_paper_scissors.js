3fd59d947d6b183cd0a63115cca964e9a368d11d
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-11-27 20:06:52 +0000

    Completion of first draft of the game. All via console.

---
+++ rock_paper_scissors.js
@@ -0,0 +1,68 @@
+function getComputerChoice(){
+    const selections = ['Rock', 'Paper', 'Scissors'];
+    let cpuSelection = selections[Math.floor(Math.random() * 3)];
+    return cpuSelection;
+}
+
+function getPlayerChoice(){
+    let playerSelection = prompt("Rock Paper or Scissors");
+    return playerSelection.toUpperCase()    ;
+}
+
+function playRound(playerSelection, computerSelection){
+    let message;
+    console.log(playerSelection);
+    if (computerSelection == "Rock"){
+        if (playerSelection == "ROCK"){
+            message = "Draw! You both chose Rock";
+        }
+        else if (playerSelection == "PAPER"){
+            message = "You Win! Paper beats Rock";
+        }
+        else if (playerSelection == "SCISSORS"){
+            message = "You Lose! Rock beats Scissors";
+        }
+        else {
+            message = "Your user selection was wrong, please select a correct choice";
+        }
+    }
+    else if (computerSelection == "Paper"){
+        if (playerSelection == "ROCK"){
+            message = "You Lose! Paper beats Rock";
+        }
+        else if (playerSelection == "PAPER"){
+            message = "Draw! You both chose Paper";
+        }
+        else if (playerSelection == "SCISSORS"){
+            message = "You Win! Scissors beats Paper";
+        }
+        else {
+            message = "Your user selection was wrong, please select a correct choice";
+        }
+    }
+    else{
+        if (playerSelection == "ROCK"){
+            message = "You Win! Rock beats Scissors.";
+        }
+        else if (playerSelection == "PAPER"){
+            message = "You Lose! Scissors beats Paper";
+        }
+        else if (playerSelection == "SCISSORS"){
+            message = "Draw! You both chose Scissors";
+        }
+        else {
+            message = "Your user selection was wrong, please select a correct choice";
+        }
+    }
+    return message;
+}
+
+function game(){
+    for (i = 0; i < 5; i++){
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+        console.log(playRound(playerSelection, computerSelection));  
+    }
+}
+
+game();
